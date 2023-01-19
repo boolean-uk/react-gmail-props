b7ac4e0f7a2c8a88ca15dc84b4b39da7a06daacb
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-01-19 15:40:29 +0000

    Took everything from the <main> tag into the Emails component. List renders successfully using props

--- src/Emails.js
+++ src/Emails.js
@@ -1,7 +1,34 @@
-function Emails() {
-    return (
-        
-
-
-    )
+function Emails(props) {
+  console.log(props);
+  return (
+    <main className="emails">
+      <ul>
+        {props.filteredEmails.map((email, index) => (
+          // This is the individual emails <Email>:
+          <li key={index} className={`email ${email.read ? "read" : "unread"}`}>
+            <div className="select">
+              <input
+                className="select-checkbox"
+                type="checkbox"
+                checked={email.read}
+                onChange={() => props.toggleRead(email)}
+              />
+            </div>
+            <div className="star">
+              <input
+                className="star-checkbox"
+                type="checkbox"
+                checked={email.starred}
+                onChange={() => props.toggleStar(email)}
+              />
+            </div>
+            <div className="sender">{email.sender}</div>
+            <div className="title">{email.title}</div>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
+
+export default Emails;
--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Emails from "./Emails";
 
 import initialEmails from "./data/emails";
 
@@ -91,37 +92,11 @@ function App() {
           </li>
         </ul>
       </nav>
-      {/* <Emails> ... Get this to work FIRST!!!!!! */}
-      <main className="emails">
-        <ul>
-          {filteredEmails.map((email, index) => (
-            // This is the individual emails <Email>:
-            <li
-              key={index}
-              className={`email ${email.read ? "read" : "unread"}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <Emails
+        filteredEmails={filteredEmails}
+        toggleRead={toggleRead}
+        toggleStar={toggleStar}
+      />
     </div>
   );
 }
