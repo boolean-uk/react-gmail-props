8d36c27da197ca70ab86a8edecc7cbd9d1545237
iliask796 <iliaskal7@gmail.com>
2023-08-18 16:16:48 +0300

    Finished Core Criteria

---
+++ src/components/Emails.js
@@ -0,0 +1,15 @@
+import '../styles/emails.css'
+
+import Email from "./Email";
+
+function Emails(props) {
+    return (
+        <ul>
+          {props.filteredEmails.map((email, index) => (
+            <Email key={index} email={email} index={index} toggleRead={props.toggleRead} toggleStar={props.toggleStar}/>
+          ))}
+        </ul>
+    );
+}
+
+export default Emails
