7ebe0dde87850636e64e46592c88995b07ed459b
Jack Miller <jdmiller1991@outlook.com>
2023-10-27 17:39:29 +0100

    Email tab done

--- src/components/Emails.jsx
+++ src/components/Emails.jsx
@@ -1,3 +1,5 @@
+import Email from "./Email.jsx";
+
 export default function Emails({ hideRead, emails, setEmails, currentTab }) {
     const toggleRead = (targetEmail) => {
         const updatedEmails = (emails) =>
@@ -29,31 +31,8 @@ export default function Emails({ hideRead, emails, setEmails, currentTab }) {
     return (
         <main className="emails">
             <ul>
-                {filteredEmails.map((email, index) => (
-                    <li
-                        key={index}
-                        className={`email ${email.read ? "read" : "unread"}`}
-                    >
-                        <div className="select">
-                            <input
-                                className="select-checkbox"
-                                type="checkbox"
-                                checked={email.read}
-                                onChange={() => toggleRead(email)}
-                            />
-                        </div>
-                        <div className="star">
-                            <input
-                                className="star-checkbox"
-                                type="checkbox"
-                                checked={email.starred}
-                                onChange={() => toggleStar(email)}
-                            />
-                        </div>
-                        <div className="sender">{email.sender}</div>
-                        <div className="title">{email.title}</div>
-                    </li>
-                ))}
+                {filteredEmails.map((email, index) => 
+                <Email key={'email'+index} email={email} toggleStar={toggleStar} toggleRead={toggleRead}/>)}  
             </ul>
         </main>
     )
