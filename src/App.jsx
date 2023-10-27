8181fd85f0cb96a66cab6ed41e7cb8715ee781d8
Lukas Dembicki <lad_777@hotmail.com>
2023-10-27 17:52:42 +0100

    Header file and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import Header from './Components/Header/Header.jsx'
 import { useState } from 'react'
 
 import initialEmails from './data/emails'
 
 import './styles/App.css'
 
-// Function to be moved to EMAILS //
+
 const getReadEmails = emails => emails.filter(email => !email.read)
-// Function to be moved to EMAILS //
+
 const getStarredEmails = emails => emails.filter(email => email.starred)
 
 function App() {
-  // State to be moved to EMAILS //
+
   const [emails, setEmails] = useState(initialEmails)
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
@@ -45,22 +46,7 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
+    <Header></Header>
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
