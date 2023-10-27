bb83e4438c708c1be043800e3b6804d551f194f8
TomEastwood <t.eastwood99@gmail.com>
2023-10-27 17:32:35 +0100

    added Header.jsx to core

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import initialEmails from './data/emails'
 import './styles/App.css'
 import Emails from './components/Emails'
 import LeftNav from './components/LeftNav'
+import Header from './components/Header'
 
 const getReadEmails = emails => emails.filter(email => !email.read)
 
@@ -46,20 +47,7 @@ function App() {
   return (
     <div className="app">
       <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
+        <Header />
       </header>
       <nav className="left-menu">
         <LeftNav 
