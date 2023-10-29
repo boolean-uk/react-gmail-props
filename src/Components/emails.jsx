ddd348201da0abb88a72ae89be7165528d7096fe
Atif Furqaan <atifurqaan@gmail.com>
2023-10-29 23:42:37 +0000

    Completed the assignment

---
+++ src/Components/emails.jsx
@@ -0,0 +1,34 @@
+import "../styles/emails.css";
+
+import Email from "./email";
+
+function Emails({ emails, toggleRead, toggleStar, hideRead, currentTab }) {
+  const getReadEmails = (emails) => emails.filter((email) => !email.read);
+  const getStarredEmails = (emails) => emails.filter((email) => email.starred);
+
+  let filteredEmails = emails;
+
+  if (hideRead) filteredEmails = getReadEmails(filteredEmails);
+
+  if (currentTab === "starred")
+    filteredEmails = getStarredEmails(filteredEmails);
+
+  return (
+    <>
+      <main className="emails">
+        <ul>
+          {filteredEmails.map((email, index) => (
+            <Email
+              key={index}
+              toggleRead={toggleRead}
+              toggleStar={toggleStar}
+              email={email}
+            />
+          ))}
+        </ul>
+      </main>
+    </>
+  );
+}
+
+export default Emails;
