f4ebc3dff1a71f4d2acc3465ddb24f7572616c45
SukunimiVinod <vinodvinod19032022@gmail.com>
2023-10-30 17:36:09 +0200

    core done

---
+++ src/components/Email.jsx
@@ -0,0 +1,35 @@
+const Email = (props) => {
+    return (
+        <li
+        // eslint-disable-next-line react/prop-types
+        key={props.index}
+        // eslint-disable-next-line react/prop-types
+        className={`email ${props.email.read ? 'read' : 'unread'}`}
+      >
+        <div className="select">
+          <input
+            className="select-checkbox"
+            type="checkbox"
+            // eslint-disable-next-line react/prop-types
+            checked={props.email.read}
+            // eslint-disable-next-line react/prop-types
+            onChange={() => props.toggleRead(props.email)}
+          />
+        </div>
+        <div className="star">
+          <input
+            className="star-checkbox"
+            type="checkbox"
+            // eslint-disable-next-line react/prop-types
+            checked={props.email.starred}
+            // eslint-disable-next-line react/prop-types
+            onChange={() => props.toggleStar(props.email)}
+          />
+        </div>
+        <div className="sender">{props.email.sender}</div>
+        <div className="title">{props.email.title}</div>
+      </li>
+    )
+}
+
+export default Email
