15f2094ce5194c4dbc79d300806f0fa5210652cc
atiqur rahman <atiq.ar@gmail.com>
2023-10-31 17:08:29 +0000

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import initialEmails from './data/emails'
-import './styles/App.css'
-
-import Header from './components/Header'
-import Emails from './components/Emails'
-import Nav from './components/Nav'
+// App.jsx
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Emails from './components/Emails';
+import Nav from './components/Nav';
+import initialEmails from './data/emails';
+import './styles/App.css';
 
 function App() {
-  const [emails, setEmails] = useState(initialEmails)
-  const [hideRead, setHideRead] = useState(false)
-  const [currentTab, setCurrentTab] = useState('inbox')
+  const [emails, setEmails] = useState(initialEmails);
+  const [hideRead, setHideRead] = useState(false);
+  const [currentTab, setCurrentTab] = useState('inbox');
 
   return (
     <div className="app">
       <Header />
-      
-      <Nav emails={emails} 
-      currentTab={currentTab} 
-      setCurrentTab={setCurrentTab} 
-      hideRead={hideRead} 
-      setHideRead={setHideRead}
+      <Nav
+        emails={emails}
+        currentTab={currentTab}
+        setCurrentTab={setCurrentTab}
+        hideRead={hideRead}
+        setHideRead={setHideRead}
       />
-
-      <Emails emails={emails} 
-      currentTab={currentTab} 
-      hideRead={hideRead} 
-      setEmails={setEmails} 
+      <Emails
+        emails={emails}
+        currentTab={currentTab}
+        hideRead={hideRead}
+        setEmails={setEmails}
       />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,47 +1,40 @@
-import "../styles/Nav.css"
+// components/Nav.jsx
+import React from 'react';
+import '../styles/Nav.css';
 
 const Nav = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const unreadEmails = props.emails.filter(email => !email.read)
-  // eslint-disable-next-line react/prop-types
-  const starredEmails = props.emails.filter(email => email.starred)
+  const unreadEmails = props.emails.filter(email => !email.read);
+  const starredEmails = props.emails.filter(email => email.starred);
 
-    return (
-        <nav className="left-menu">
-        <ul className="inbox-list">
-          <li
-            // eslint-disable-next-line react/prop-types
-            className={`item ${props.currentTab === 'inbox' ? 'active' : ''}`}
-            // eslint-disable-next-line react/prop-types
-            onClick={() => props.setCurrentTab('inbox')}
-          >
-            <span className="label">Inbox</span>
-            <span className="count">{unreadEmails.length}</span>
-          </li>
-          <li
-            // eslint-disable-next-line react/prop-types
-            className={`item ${props.currentTab === 'starred' ? 'active' : ''}`}
-            // eslint-disable-next-line react/prop-types
-            onClick={() => props.setCurrentTab('starred')}
-          >
-            <span className="label">Starred</span>
-            <span className="count">{starredEmails.length}</span>
-          </li>
+  return (
+    <nav className="left-menu">
+      <ul className="inbox-list">
+        <li
+          className={`item ${props.currentTab === 'inbox' ? 'active' : ''}`}
+          onClick={() => props.setCurrentTab('inbox')}
+        >
+          <span className="label">Inbox</span>
+          <span className="count">{unreadEmails.length}</span>
+        </li>
+        <li
+          className={`item ${props.currentTab === 'starred' ? 'active' : ''}`}
+          onClick={() => props.setCurrentTab('starred')}
+        >
+          <span className="label">Starred</span>
+          <span className="count">{starredEmails.length}</span>
+        </li>
+        <li className="item toggle">
+          <label htmlFor="hide-read">Hide read</label>
+          <input
+            id="hide-read"
+            type="checkbox"
+            checked={props.hideRead}
+            onChange={(e) => props.setHideRead(e.target.checked)}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-          <li className="item toggle">
-            <label htmlFor="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              // eslint-disable-next-line react/prop-types
-              checked={props.hideRead}
-              // eslint-disable-next-line react/prop-types
-              onChange={e => props.setHideRead(e.target.checked)}
-            />
-          </li>
-        </ul>
-      </nav>
-    )
-}
-
-export default Nav
+export default Nav;
