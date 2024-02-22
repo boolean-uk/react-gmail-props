548ec695d68096d75b3c8496d9a6a267afc71181
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-22 11:03:56 +0100

    finished core

---
+++ src/Email.jsx
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+function Email({ email, toggleRead, toggleStar }) {
+  return (
+    <li className={`email ${email.read ? "read" : "unread"}`}>
+      <div className="select">
+        <input
+          className="select-checkbox"
+          type="checkbox"
+          checked={email.read}
+          onChange={() => toggleRead(email)}
+        />
+      </div>
+      <div className="star">
+        <input
+          className="star-checkbox"
+          type="checkbox"
+          checked={email.starred}
+          onChange={() => toggleStar(email)}
+        />
+      </div>
+      <div className="sender">{email.sender}</div>
+      <div className="title">{email.title}</div>
+    </li>
+  );
+}
+
+Email.propTypes = {
+  email: PropTypes.object,
+  toggleRead: PropTypes.func,
+  toggleStar: PropTypes.func,
+};
+
+export default Email;
