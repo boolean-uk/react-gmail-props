00f39ad5f3e0babc37e7b6667c092976a1570508
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-22 14:27:19 +0100

    finished extension 1

--- src/Email.jsx
+++ src/Email.jsx
@@ -1,25 +1,50 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+
 function Email({ email, toggleRead, toggleStar }) {
+  const [displayContent, setDisplayContent] = useState(false);
+
+  const handleTitleClick = () => {
+    setDisplayContent(true);
+  };
+
+  const handleBackClick = () => {
+    setDisplayContent(false);
+  };
+
   return (
     <li className={`email ${email.read ? "read" : "unread"}`}>
-      <div className="select">
-        <input
-          className="select-checkbox"
-          type="checkbox"
-          checked={email.read}
-          onChange={() => toggleRead(email)}
-        />
-      </div>
-      <div className="star">
-        <input
-          className="star-checkbox"
-          type="checkbox"
-          checked={email.starred}
-          onChange={() => toggleStar(email)}
-        />
-      </div>
-      <div className="sender">{email.sender}</div>
-      <div className="title">{email.title}</div>
+      {!displayContent ? (
+        <>
+          <div className="select">
+            <input
+              className="select-checkbox"
+              type="checkbox"
+              checked={email.read}
+              onChange={() => toggleRead(email)}
+            />
+          </div>
+          <div className="star">
+            <input
+              className="star-checkbox"
+              type="checkbox"
+              checked={email.starred}
+              onChange={() => toggleStar(email)}
+            />
+          </div>
+          <div className="sender">{email.sender}</div>
+          <div className="title" onClick={handleTitleClick}>
+            {email.title}
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="back-button" onClick={handleBackClick}>
+            Back
+          </div>
+          <div className="content">{email.content}</div>
+        </>
+      )}
     </li>
   );
 }
