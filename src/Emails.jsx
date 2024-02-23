2680bdeadf5641a9d1ec5bde5ee3cf39564ddb66
henrikrosenkilde <henrik.rosenkilde@hotmail.com>
2024-02-23 23:04:54 +0100

    core

---
+++ src/Emails.jsx
@@ -0,0 +1,17 @@
+import Email from './Email'
+
+//emails component
+function Emails({ emails, filteredEmails, toggleRead, toggleStar }) {
+    return (
+      <main className="emails">
+        <ul>
+          {/* Render individual email items */}
+          {filteredEmails.map((email, index) => (
+            <Email key={index} email={email} toggleRead={toggleRead} toggleStar={toggleStar} />
+          ))}
+        </ul>
+      </main>
+    );
+  }
+
+  export default Emails;
