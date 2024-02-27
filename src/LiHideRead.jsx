be39b06edd6b678f20116a35bc839763a75a19c1
Knut S Rostad <knut.rostad@no.experis.com>
2024-02-27 13:32:54 +0100

    Core done

---
+++ src/LiHideRead.jsx
@@ -0,0 +1,15 @@
+
+function LiHideRead(props){
+    return(
+        <li className="item toggle">
+          <label htmlFor="hide-read">Hide read</label>
+          <input
+            id="hide-read"
+            type="checkbox"
+            checked={props.hideRead}
+            onChange={e => props.setHideRead(e.target.checked)}
+          />
+        </li>
+    )
+}
+export default LiHideRead
---
+++ src/Nav.jsx
@@ -0,0 +1,28 @@
+import LiInbox from './LiInbox.jsx'
+import LiStarred from './LiStarred.jsx'
+import LiHideRead from './LiHideRead.jsx'
+
+function Nav(props){
+  
+  const unreadEmails = props.emails.filter(email => !email.read)
+  const starredEmails = props.emails.filter(email => email.starred)
+  
+  return (
+      <nav className="left-menu">
+      <ul className="inbox-list">
+        <LiInbox currentTab={props.currentTab}
+         setCurrentTab={props.setCurrentTab}
+         unreadEmails={unreadEmails}
+         />
+         
+        <LiStarred currentTab={props.currentTab} setCurrentTab={props.setCurrentTab}
+         starredEmails={starredEmails}/>
+
+        <LiHideRead hideRead={props.hideRead}
+         setHideRead={props.setHideRead} 
+         />
+      </ul>
+    </nav>
+  )
+}
+export default Nav
