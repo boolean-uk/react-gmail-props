4c4ccfb182eb4c7504559d34c3fd9ee2742e9993
Nicolai Klokmose <angryniczorn@gmail.com>
2024-02-27 13:08:00 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 import initialEmails from './data/emails'
+import Header from './Header.jsx'
+import Leftmenu from './Leftmenu.jsx'
 
 import './styles/App.css'
 
@@ -43,50 +45,16 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
-      <nav className="left-menu">
-        <ul className="inbox-list">
-          <li
-            className={`item ${currentTab === 'inbox' ? 'active' : ''}`}
-            onClick={() => setCurrentTab('inbox')}
-          >
-            <span className="label">Inbox</span>
-            <span className="count">{unreadEmails.length}</span>
-          </li>
-          <li
-            className={`item ${currentTab === 'starred' ? 'active' : ''}`}
-            onClick={() => setCurrentTab('starred')}
-          >
-            <span className="label">Starred</span>
-            <span className="count">{starredEmails.length}</span>
-          </li>
-
-          <li className="item toggle">
-            <label htmlFor="hide-read">Hide read</label>
-            <input
-              id="hide-read"
-              type="checkbox"
-              checked={hideRead}
-              onChange={e => setHideRead(e.target.checked)}
-            />
-          </li>
-        </ul>
-      </nav>
+     <Header />
+     <Leftmenu 
+        currentTab={currentTab} 
+        setCurrentTab={setCurrentTab} 
+        unreadEmails={unreadEmails} 
+        starredEmails={starredEmails}
+        setHideRead={setHideRead}
+        hideRead={hideRead}
+        />
+     
       <main className="emails">
         <ul>
           {filteredEmails.map((email, index) => (
