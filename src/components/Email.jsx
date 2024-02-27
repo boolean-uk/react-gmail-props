5f2a3f248509185fa0a5df2d0b4fc179702b84ba
Maha Riazi <maha.riazi@hotmail.no>
2024-02-27 12:29:03 +0100

    split styles for components

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types'
 
+import '../styles/Email.css'
+
 function Email({ email, toggleRead, toggleStar }) {
     return (
       <li className={`email ${email.read ? "read" : "unread"}`}>
--- src/components/Emails.jsx
+++ src/components/Emails.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import Email from "./Email";
 
+import '../styles/Emails.css'
+
 function Emails({ filteredEmails, toggleRead, toggleStar }) {
   return (
     <main className="emails">
