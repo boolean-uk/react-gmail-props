44db41fc2ad42c0e1b9ec3c9f5e2f7f5a17b23d6
Gustav Svennas <gustavsvennas@gmail.com>
2024-02-27 16:21:57 +0100

    Done with core. Will go back and do extension when I have time over.

---
+++ src/Components/Emails.jsx
@@ -0,0 +1,20 @@
+import Email from "./Email";
+
+function Emails(props) {
+  return (
+    <main className="emails">
+      <ul>
+        {props.filteredEmails.map((email, index) => (
+          <Email
+            key={index}
+            email={email}
+            toggleRead={props.toggleRead}
+            toggleStar={props.toggleStar}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+export default Emails;
