6f7517dbfca935e51a8e3bc1941fa2275cafb95c
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-02-27 13:15:25 +0100

    Header component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import initialEmails from "./data/emails";
 
 import "./styles/App.css";
 import Emails from "./Emails";
+import Header from "./Header";
 
 function App() {
   const [emails, setEmails] = useState(initialEmails);
@@ -15,22 +16,7 @@ function App() {
 
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
+      <Header />
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
