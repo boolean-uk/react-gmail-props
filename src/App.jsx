a7eb56a0b69070fc2af26d81c92b666453c134f4
tonnesegeland <to.smith.eg@gmail.com>
2024-02-27 14:30:28 +0100

    Core

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 import initialEmails from './data/emails'
 
 import './styles/App.css'
+import Header from './Components/Header'
+import Emails from './Components/Emails'
 
 const getReadEmails = emails => emails.filter(email => !email.read)
 
@@ -12,7 +14,10 @@ function App() {
   const [emails, setEmails] = useState(initialEmails)
   const [hideRead, setHideRead] = useState(false)
   const [currentTab, setCurrentTab] = useState('inbox')
+  
 
+
+  // Passed into emails component
   const unreadEmails = emails.filter(email => !email.read)
   const starredEmails = emails.filter(email => email.starred)
 
@@ -41,24 +46,16 @@ function App() {
   if (currentTab === 'starred')
     filteredEmails = getStarredEmails(filteredEmails)
 
+
+
+
+
+
   return (
     <div className="app">
-      <header className="header">
-        <div className="left-menu">
-          <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-            <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-          </svg>
-
-          <img
-            src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-            alt="gmail logo"
-          />
-        </div>
-
-        <div className="search">
-          <input className="search-bar" placeholder="Search mail" />
-        </div>
-      </header>
+      
+      < Header />
+    
       <nav className="left-menu">
         <ul className="inbox-list">
           <li
@@ -87,35 +84,8 @@ function App() {
           </li>
         </ul>
       </nav>
-      <main className="emails">
-        <ul>
-          {filteredEmails.map((email, index) => (
-            <li
-              key={index}
-              className={`email ${email.read ? 'read' : 'unread'}`}
-            >
-              <div className="select">
-                <input
-                  className="select-checkbox"
-                  type="checkbox"
-                  checked={email.read}
-                  onChange={() => toggleRead(email)}
-                />
-              </div>
-              <div className="star">
-                <input
-                  className="star-checkbox"
-                  type="checkbox"
-                  checked={email.starred}
-                  onChange={() => toggleStar(email)}
-                />
-              </div>
-              <div className="sender">{email.sender}</div>
-              <div className="title">{email.title}</div>
-            </li>
-          ))}
-        </ul>
-      </main>
+
+      < Emails filteredEmails={filteredEmails} toggleRead={toggleRead} toggleStar={toggleStar}/>
     </div>
   )
 }
---
+++ src/Components/Emails.jsx
@@ -0,0 +1,21 @@
+import Email from './EmailComponent';
+
+function Emails( { filteredEmails, toggleRead, toggleStar} ) {
+    return (  
+<main className="emails">
+        <ul>
+          {filteredEmails.map((email, index) => (
+            < Email
+                key={index}
+                email={email}
+                toggleRead={toggleRead}
+                toggleStar={toggleStar}
+            />
+
+          ))}
+        </ul>
+      </main>
+    );
+}
+
+export default Emails;
