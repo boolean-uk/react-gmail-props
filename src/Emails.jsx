89e7758ce1f00c001d7b22d724a6ebcdc54c4765
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-02-27 13:13:02 +0100

    Emails and Email component, added prop-types.

---
+++ src/Emails.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import Email from "./Email";
+
+const getReadEmails = (emails) => emails.filter((email) => !email.read);
+
+const getStarredEmails = (emails) => emails.filter((email) => email.starred);
+
+function Emails(props) {
+  let filteredEmails = props.emails;
+
+  if (props.hideRead) filteredEmails = getReadEmails(filteredEmails);
+
+  if (props.currentTab === "starred")
+    filteredEmails = getStarredEmails(filteredEmails);
+
+  return (
+    <main className="emails">
+      <ul>
+        {filteredEmails.map((email, index) => (
+          <Email
+            key={index}
+            email={email}
+            index={index}
+            setEmails={props.setEmails}
+          />
+        ))}
+      </ul>
+    </main>
+  );
+}
+
+Emails.propTypes = {
+  emails: PropTypes.array,
+  hideRead: PropTypes.bool,
+  currentTab: PropTypes.string,
+  setEmails: PropTypes.func,
+};
+
+export default Emails;
